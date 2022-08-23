08e973fc4503fcec76720dc2785a4f5198c6cd57
Jimyung Koh <kojimyung1206@naver.com>
2022-08-23 20:40:56 +0900

    style: fix comments to comply with JSDoc style

--- service/jobApplicationService.js
+++ service/jobApplicationService.js
@@ -3,6 +3,8 @@ const JobApplication = require('../models/jobApplication')(sequelize);
 
 /**
  * 요구사항 6. 사용자가 채용공고에 지원하기
+ * @param {Object} info
+ * @returns {Object}
  */
 exports.enrollApplicant = async (info) => {
   const jobApplication = {
