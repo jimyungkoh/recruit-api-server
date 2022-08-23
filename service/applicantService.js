78496aecf619a05fb0fb7843968c27a9405b56df
Jimyung Koh <kojimyung1206@naver.com>
2022-08-23 17:26:38 +0900

    feat: add a function validating applicant data's existence

---
+++ service/applicantService.js
@@ -0,0 +1,28 @@
+const {sequelize} = require('../models');
+const {NotFoundError} = require('../components/errors');
+const Applicant = require('../models/applicant')(sequelize);
+
+/**
+ * 등록된 모든 지원자 조회 메서드
+ */
+exports.getAll = async () => {
+  const applicants = await Applicant.findAll();
+
+  return JSON.stringify(applicants);
+};
+
+/**
+ * 회사 id 유효성을 검사
+ * @param {number} applicant_id
+ */
+exports.validateApplicantId = async (applicant_id) => {
+  const applicant = await Applicant.findOne({
+    where: {
+      id: applicant_id
+    }
+  });
+
+  if (!applicant) {
+    throw new NotFoundError(`${applicant_id} doesn't exist in company table.`);
+  }
+}
