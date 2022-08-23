63877481fb7ed8a42fda0c514fbd36325504ebf1
Jimyung Koh <kojimyung1206@naver.com>
2022-08-24 08:10:12 +0900

    chore: swagger version up from 2.0 to 3.0.3

--- swagger.js
+++ swagger.js
@@ -1,7 +1,5 @@
 const {openingApi} = require("./api");
-const swaggerAutogen = require("swagger-autogen")();
 
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./api/company.js', './api/jobApplication.js', './api/opening.js'];
 
-swaggerAutogen(outputFile, endpointsFiles);
