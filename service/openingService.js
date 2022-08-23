2b8799ff91b598abb546c5632a897be876d60425
Jimyung Koh <kojimyung1206@naver.com>
2022-08-23 20:42:29 +0900

    feat: add a function, which do update opening table

--- service/openingService.js
+++ service/openingService.js
@@ -1,7 +1,6 @@
 const {sequelize} = require('../models');
 const {NotFoundError} = require('../components/errors');
 const Opening = require('../models/opening')(sequelize);
-const {Op, Sequelize} = require('sequelize');
 
 /**
  * 요구사항 1. 채용공고 등록하기
@@ -22,6 +21,32 @@ exports.postOpening = async (info) => {
   return Opening.create(opening);
 };
 
+
+/**
+ * 요구사항 2. 채용공고 수정하기
+ * @param {number} id
+ * @param {Object} contents
+ * @returns {Object}
+ */
+exports.updateOpening = async (id, contents) => {
+  if (contents.id || contents.company_id) {
+    throw new BadRequestError('id value cannot be \'modified\'.');
+  }
+
+  await Opening.update(contents, {
+    where: {
+      id: id
+    }
+  });
+
+  const opening = await Opening.findOne({where: {id: id}})
+
+  if (!opening) {
+    throw new NotFoundError('채용공고를 찾을 수 없습니다.');
+  }
+
+  return opening;
+}
 
 /**
  * 채용공고 id 유효성을 검사
